503a45131e9f6fcb83fc0552d9a0c79405845af8
Sarah Noor <88581241+sarah-noor-12232@users.noreply.github.com>
2023-12-08 10:35:53 +0000

    applying model

---
+++ dataloaders/the_coding_bug_obj_detection.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8n.pt')  # load an official model
+# model = YOLO('path/to/best.pt')  # load a custom model
+
+# Predict with the model
+results = model('./Dataset/UCF101-n-frames')  # predict on an images
+
+#hello hania, :D 
+#HI SARAH
+
+#HEHEHE
+#lol
