7e8abfd85d12df7d7372ab44bbd6eb64154dae16
hk819 <80596819+hk819@users.noreply.github.com>
2023-12-10 02:50:54 +0500

    tried to train model but not working

--- dataloaders/Defining_Action_Hania9.py
+++ dataloaders/Defining_Action_Hania9.py
@@ -4,11 +4,11 @@ import os
 import shutil
 
 # Define the source folder containing all action folders
-source_folder = '/Dataset/UCF-yolo'
+source_folder = './Dataset/UCF-yolo'
 
 # Define the destination folders for normal and abnormal actions
-normal_action_folder = '/Dataset/NormalAction'
-abnormal_action_folder = '/Dataset/AbnormalAction'
+normal_action_folder = './Action/NormalAction'
+abnormal_action_folder = './Action/AbnormalAction'
 
 # Create destination folders if they don't exist
 os.makedirs(normal_action_folder, exist_ok=True)
@@ -40,62 +40,3 @@ for action_folder in os.listdir(source_folder):
         print(f"Ignoring {action_folder}.")
 
 print("Copy process completed.")
-
-
-
-
-
-#-------------------------------------Feature Enginerring----------------------------
-from ultralytics.engine import results
-import torch
-
-# Example results for Normal Action
-normal_action_result = results.Boxes(
-    cls=torch.tensor([0.]),
-    conf=torch.tensor([0.8754]),
-    data=torch.tensor([[79.0000, 0.0000, 320.0000, 240.0000, 0.8754, 0.0000]]),
-    id=None,
-    is_track=False,
-    orig_shape=(240, 320),
-    shape=torch.Size([1, 6]),
-    xywh=torch.tensor([[199.5000, 120.0000, 241.0000, 240.0000]]),
-    xywhn=torch.tensor([[0.6234, 0.5000, 0.7531, 1.0000]]),
-    xyxy=torch.tensor([[79., 0., 320., 240.]]),
-    xyxyn=torch.tensor([[0.2469, 0.0000, 1.0000, 1.0000]])
-)
-
-# Example results for Abnormal Action
-abnormal_action_result = results.Boxes(
-    cls=torch.tensor([0.]),
-    conf=torch.tensor([0.8260]),
-    data=torch.tensor([[35.0000, 71.0000, 206.0000, 240.0000, 0.8260, 0.0000]]),
-    id=None,
-    is_track=False,
-    orig_shape=(240, 320),
-    shape=torch.Size([1, 6]),
-    xywh=torch.tensor([[120.5000, 155.5000, 171.0000, 169.0000]]),
-    xywhn=torch.tensor([[0.3766, 0.6479, 0.5344, 0.7042]]),
-    xyxy=torch.tensor([[35., 71., 206., 240.]]),
-    xyxyn=torch.tensor([[0.1094, 0.2958, 0.6438, 1.0000]])
-)
-
-# Function to extract features
-def extract_features(result):
-    features = {
-        'class_index': result.cls.item(),
-        'confidence': result.conf.item(),
-        'normalized_bbox': result.xywhn.squeeze().tolist(),
-        'original_bbox': result.xyxy.squeeze().tolist(),
-        # Add more features as needed
-    }
-    return features
-
-# Extract features for Normal Action
-normal_action_features = extract_features(normal_action_result)
-print("Features for Normal Action:")
-print(normal_action_features)
-
-# Extract features for Abnormal Action
-abnormal_action_features = extract_features(abnormal_action_result)
-print("\nFeatures for Abnormal Action:")
-print(abnormal_action_features)
--- dataloaders/train.py
+++ dataloaders/train.py
@@ -1,9 +1,4 @@
-!yolo task=detect mode=train model=yolov8s.pt data={dataset.location}/data.yaml epochs=25 imgsz=800 plots=True
-
-
-
-
-
+# !yolo task=detect mode=train model=yolov8s.pt data={dataset.location}/data.yaml epochs=25 imgsz=800 plots=True
 
 
 
@@ -11,26 +6,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolov8n-pose.yaml').load('yolov8n.pt')  # load a pretrained model
+model = YOLO('yolov8n-pose')  # load a pretrained model
 
 # Train the model
-results = model.train(data='coco128.yaml', epochs=100, imgsz=640)
-
-
-
-
-
-import os
-from ultralytics import YOLO
-
-# Define the path to your YOLOv8n-pose configuration file
-config_path = 'path/to/yolov8n-pose.yaml'
-
-# Load the YOLO model with the specified configuration file
-model = YOLO(config_path)
-
-# Define the path to your UCF.yaml file
-ucf_config_path = 'path/to/UCF.yaml'
-
-# Train the model using the specified UCF.yaml file
-results = model.train(data=ucf_config_path, epochs=100, imgsz=640)
+model.train(data='custom_data.yaml', batch=8, epochs=100, imgsz=640)
